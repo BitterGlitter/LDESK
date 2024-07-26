6166bc255d233b8f317c237d4c95a22918d687be
Apurv <apurvsikka@gmail.com>
2024-07-26 22:41:49 +0530

    feat: add hidden about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,31 @@
 <template>
   <div>
-    
+      <div class="bg-white py-24 sm:py-32">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+          <div class="mx-auto max-w-2xl lg:max-w-none">
+            <div class="text-center">
+              <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">About The Makers</h2>
+              <p class="mt-4 text-lg leading-8 text-gray-600">
+                LDESK is a side project by three passionate self-taught web developers , to speed up the process of teacher mangement and adjustment in institutions.
+              </p>
+            </div>
+            <dl class="mt-16 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-3 lg:grid-cols-3">
+              <div class="flex flex-col bg-gray-200/50 p-8">
+                <dt class="text-sm font-semibold leading-6 text-gray-600"><a action="_blank" href="https://github.com/apurvsikka/"> Apurv Sikka </a></dt>
+                <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900">Frontend Developer</dd>
+              </div>
+              <div class="flex flex-col bg-gray-200/50 p-8">
+                <dt class="text-sm font-semibold leading-6 text-gray-600"><a action="_blank" href="https://github.com/VAibhavSys/">Vaibhav Dhiman</a></dt>
+                <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900">Backend Developer</dd>
+              </div>
+              <div class="flex flex-col bg-gray-200/50 p-8">
+                <dt class="text-sm font-semibold leading-6 text-gray-600">Naman Kamboj</dt>
+                <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900">Database and Email</dd>
+              </div>
+            </dl>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
